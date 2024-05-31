6860ca319e8278f23b33862f8aebf821f31c7d6f
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-05-31 22:50:35 +0300

    add _document

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,6 +11,13 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        
       <Home/>
     </div>
   );
