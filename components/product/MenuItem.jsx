cab91f63a19b8a8463248327da4570cec4b6c888
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-06-02 22:31:05 +0300

    added some of pics

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -6,21 +6,20 @@ const MenuItem = () => {
     <div className="bg-secondary rounded-3xl">
       <div className="w-full bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
         <div className="relative w-36 h-36 hover:scale-110 transition-all">
-          <Image src="/images/f1.png" alt="" layout="fill" className=""/>
+          <Image src="/images/f1.png" alt="" layout="fill"/>
         </div>
       </div>
       <div className="p-[25px] text-white ">
         <h4 className="text-xl font-semibold">Delicious Pizza</h4>
         <p className="text-[15px]">
-          Thin crust, tomato sauce, melted mozzarella, assorted toppings—a burst
-          of flavor in every bite.
+          Thin crust, tomato sauce, melted mozzarella, assorted toppings a burst of flavor in every bite.
         </p>
         <div className="flex justify-between items-center mt-4">
           <span>$20</span>
           <button className="btn-primary !w-10 !h-10 !rounded-full !p-0 grid place-content-center">
             <RiShoppingCart2Fill />
           </button>
-        </div>
+        </div> 
       </div>
     </div>
   );
