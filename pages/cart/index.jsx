ec14564f3892c1abb61c47a09dc8c3df69b31964
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-06-08 16:17:26 +0300

    Cart css

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -1,10 +1,47 @@
+import Title from "@/components/ui/Title";
+import Image from "next/image";
 
 const Cart = () => {
   return (
-    <div>
-      cart
+    <div className="min-h-[calc(100vh_-_433px)]">
+      <div className="flex justify-between">
+        <div className="min-h-[calc(100vh_-_433px)] flex items-center flex-1">
+          <table>
+            <thead>
+              <tr>
+                <th>PRODUCT</th>
+                <th>EXTRAS</th>
+                <th>PRICE</th>
+                <th>QUANTITY</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <Image src="/images/f1.png" alt="" width={50} height={50} />
+                  <span> Good Pizza</span>
+                </td>
+                <td>
+                  <span>Ketchup, Mayonnaise, Hot Sauce</span>
+                </td>
+                <td>$20</td>
+                <td>1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex felx-col justify-center text-white p-12">
+          <Title addClass="text-[40px]">CART TOTAL</Title>
+          <div className="mt-6">
+            <b>Subtotal: </b>$20 <br />
+            <b className=" inline-block my-1">Discount: </b>$0.00 <br />
+            <b>Total: </b>$20
+          </div>
+          <button className="btn-primary mt-4">CHECKOUT NOW!</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
