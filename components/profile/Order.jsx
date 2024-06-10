22552d065a082ac0578ba41c020e13aa1738b79d
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-06-10 12:15:09 +0300

    create admib product component

--- components/profile/Order.jsx
+++ components/profile/Order.jsx
@@ -4,7 +4,7 @@ import Title from '../ui/Title'
 const Order = () => {
   return (
     <div className="lg:p-8 flex-1 lg:mt-0 mt-5">
-      <Title addClass="text-[40px]">Password</Title>
+      <Title addClass="text-[40px]">Orders</Title>
       <div className="overflow-x-auto w-full mt-5">
         <table className="w-full text-sm text-center text-gray-500 min-w-[1000px]">
           <thead className="text-xs text-gray-400 uppercase bg-gray-700">
