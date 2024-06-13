bde64863669b332b3a55bc4f24d92f389f093934
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-06-13 16:29:43 +0300

    Register API req

--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -1,13 +1,20 @@
 import Input from "@/components/form/Input";
 import Title from "@/components/ui/Title";
 import { registerSchema } from "@/schema/register";
+import axios from "axios";
 import { useFormik } from "formik";
 import Link from "next/link";
 
 const Register = () => {
   const onSubmit = async (values, actions) => {
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-    actions.resetForm();
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/register`,
+        values
+      );
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
@@ -78,7 +85,9 @@ const Register = () => {
           ))}
         </div>
         <div className="flex flex-col w-full gap-y-3 mt-6">
-          <button className="btn-primary">REGISTER</button>
+          <button className="btn-primary" type="submit">
+            REGISTER
+          </button>
           <Link href="/auth/login">
             <span className="text-sm underline cursor-pointer text-secondary ">
               Do you have a account?
