132b22c083ba1c841c84651be98aff6f8b63b3c8
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-06-24 22:49:52 +0300

    edit menu category

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,22 +1,22 @@
-import Reservation from '@/components/Reservation'
-import Customers from '@/components/customers/Customers'
-import MenuWrapper from '@/components/product/MenuWrapper'
-import About from '@/components/ui/About'
-import Campaigns from '@/components/ui/Campaigns'
-import Carousel from '@/components/ui/Carousel'
-import React from 'react'
+import Reservation from "@/components/Reservation";
+import Customers from "@/components/customers/Customers";
+import MenuWrapper from "@/components/product/MenuWrapper";
+import About from "@/components/ui/About";
+import Campaigns from "@/components/ui/Campaigns";
+import Carousel from "@/components/ui/Carousel";
+import React from "react";
 
-const Index = () => {
+const Index = ({ categoryList }) => {
   return (
     <React.Fragment>
-      <Carousel/>
-      <Campaigns/>
-      <MenuWrapper/>
-      <About/>
-      <Reservation/>
-      <Customers/>
+      <Carousel />
+      <Campaigns />
+      <MenuWrapper categoryList={categoryList} />
+      <About />
+      <Reservation />
+      <Customers />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
