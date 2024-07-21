67cb4f8f007bb7d1042606fc8d4df0a774a368b5
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-07-21 12:41:37 +0300

    add progress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,15 @@ import { Provider } from "react-redux";
 import store from "@/redux/store";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
+import "../styles/globals.css";
+import "nprogress/nprogress.css";
 import "react-toastify/dist/ReactToastify.css";
+import NProgress from "nprogress"; 
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({
   Component,
