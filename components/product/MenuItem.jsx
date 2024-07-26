c0c9f20bf9a2bcac7a0a64ba80f0cb267f790657
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-07-26 21:49:15 +0300

    discount product

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -26,7 +26,13 @@ const MenuItem = ({ product }) => {
       <div className="w-full bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
         <Link href={`/product/${product._id}`}>
           <div className="relative w-36 h-36 hover:scale-110 transition-all">
-            <Image src={product.img} alt="" fill  sizes="(max-width: 640px) 100px, (max-width: 1024px) 150px, 200px"  priority />
+            <Image
+              src={product.img}
+              alt=""
+              fill
+              sizes="(max-width: 640px) 100px, (max-width: 1024px) 150px, 200px"
+              priority
+            />
           </div>
         </Link>
       </div>
@@ -34,8 +40,20 @@ const MenuItem = ({ product }) => {
         <h4 className="text-xl font-semibold">{product.title}</h4>
         <p className="text-[15px]">{product.desc}</p>
         <div className="flex justify-between items-center mt-4">
-          <span>${product.prices[0]}</span>
-        <button
+          {product.discount ? (
+            <div className="flex  items-start">
+              <span className="line-through text-red-500 mr-2 text-sm">
+                ${product.prices[0]}
+              </span>
+              <span>
+                ${(product.prices[0] * (1 - product.discount / 100)).toFixed(2)}
+              </span>
+            </div>
+          ) : (
+            <span>${product.prices[0]}</span>
+          )}
+
+          <button
             className="btn-primary !w-10 !h-10 !rounded-full !p-0 grid place-content-center"
             disabled={findCart}
             onClick={handleClick}
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,11 +1,12 @@
 import Title from "@/components/ui/Title";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addProduct } from "../../redux/cartSlice";
 import axios from "axios";
 
 const Index = ({ food }) => {
+  console.log(food)
   const [prices, setPrices] = useState(food.prices);
   const [price, setPrice] = useState(prices[0]);
   const [size, setSize] = useState(0);
@@ -14,6 +15,15 @@ const Index = ({ food }) => {
   const cart = useSelector((state) => state.cart);
   const findCart = cart.products.find((item) => item._id === food._id);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (food.discount) {
+      const discountedPrice = prices[size] * (1 - food.discount / 100);
+      setPrice(discountedPrice);
+    } else {
+      setPrice(prices[size]);
+    }
+  }, [size, prices, food.discount]);
 
   const handleSize = (sizeIndex) => {
     const difference = prices[sizeIndex] - prices[size];
@@ -42,47 +52,68 @@ const Index = ({ food }) => {
   return (
     <div className="flex items-center md:h-[calc(100vh_-_88px)] gap-5 py-20 flex-wrap">
       <div className="relative md:flex-1 md:w-[80%] md:h-[80%] w-36 h-36 mx-auto">
-        <Image src={food?.img} alt="" fill style={{ objectFit: 'contain' }} priority />
+        <Image
+          src={food?.img}
+          alt=""
+          fill
+          style={{ objectFit: "contain" }}
+          priority
+        />
       </div>
       <div className="md:flex-1 md:text-start text-center">
         <Title addClass="text-6xl">{food?.title}</Title>
-        <span className="text-primary text-2xl font-bold underline underline-offset-1 my-4 inline-block">
-          ${price}
-        </span>
+        <div className="my-4">
+          {food.discount ? (
+            <div className="flex">
+              <span className="line-through text-red-500 mr-2 font-bold">
+                ${prices[size]}
+              </span>
+              <span className="text-primary text-2xl font-bold">
+                ${price.toFixed(2)}
+              </span>
+            </div>
+          ) : (
+            <span className="text-primary text-2xl font-bold">
+              ${price.toFixed(2)}
+            </span>
+          )}
+        </div>
         <p className="text-sm my-4 md:pr-24">{food?.desc}</p>
         <div>
-          <h4 className="text-xl font-bold">Choose the size</h4>
           {food.category === "pizza" && (
-            <div className="flex items-center gap-x-20 md:justify-start justify-center">
-              <div
-                className="relative w-8 h-8 cursor-pointer"
-                onClick={() => handleSize(0)}
-              >
-                <Image src="/images/size.png" alt="" fill />
-                <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
-                  Small
-                </span>
-              </div>
+            <>
+              <h4 className="text-xl font-bold">Choose the size</h4>
+              <div className="flex items-center gap-x-20 md:justify-start justify-center">
+                <div
+                  className="relative w-8 h-8 cursor-pointer"
+                  onClick={() => handleSize(0)}
+                >
+                  <Image src="/images/size.png" alt="" fill />
+                  <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
+                    Small
+                  </span>
+                </div>
 
-              <div
-                className="relative w-12 h-12 cursor-pointer"
-                onClick={() => handleSize(1)}
-              >
-                <Image src="/images/size.png" alt="" fill />
-                <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
-                  Medium
-                </span>
+                <div
+                  className="relative w-12 h-12 cursor-pointer"
+                  onClick={() => handleSize(1)}
+                >
+                  <Image src="/images/size.png" alt="" fill />
+                  <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
+                    Medium
+                  </span>
+                </div>
+                <div
+                  className="relative w-16 h-16 cursor-pointer"
+                  onClick={() => handleSize(2)}
+                >
+                  <Image src="/images/size.png" alt="" fill />
+                  <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
+                    Large
+                  </span>
+                </div>
               </div>
-              <div
-                className="relative w-16 h-16 cursor-pointer"
-                onClick={() => handleSize(2)}
-              >
-                <Image src="/images/size.png" alt="" fill />
-                <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
-                  Large
-                </span>
-              </div>
-            </div>
+            </>
           )}
         </div>
         <div className="flex gap-x-4 my-6 md:justify-start justify-center">
