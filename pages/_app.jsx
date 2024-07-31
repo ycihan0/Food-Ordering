299e4cdfb3ea35eea20f2aad22711fbd2499c5db
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-07-31 14:18:07 +0300

    bug fixed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/globals.css";
-import Layout from "/layout/layout";
+import Layout from "/layout/Layout";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 import { SessionProvider } from "next-auth/react";
